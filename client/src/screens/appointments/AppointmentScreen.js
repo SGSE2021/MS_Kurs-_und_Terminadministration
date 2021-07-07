0e1b8aae93a714a6a5a9d1c2cc461f959ac0db6e
b-franklin <benjamin.franke@fh-bielefeld.de>
2021-07-07 15:44:09 +0200

    Removed "alle Termine" Button

--- client/src/screens/appointments/AppointmentScreen.js
+++ client/src/screens/appointments/AppointmentScreen.js
@@ -1,6 +1,6 @@
 import { Button } from "@material-ui/core";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import EnhancedTable from "../../components/EnhancedTable";
 
 const AppointmentScreen = () => {
